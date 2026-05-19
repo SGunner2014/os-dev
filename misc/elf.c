a437d6ce92ed0abdddb363a7204965eaae014a75
Sam Gunner <me@sgunner.me>
2026-05-19 21:14:46 +0100

    WIP Elf

---
+++ misc/elf.c
@@ -0,0 +1,45 @@
+#include "elf.h"
+
+bool check_elf_header(Elf32_Ehdr *hdr)
+{
+    if (!hdr)
+        return false;
+
+    if (
+        hdr->e_ident[EI_MAG0] != ELFMAG0 ||
+        hdr->e_ident[EI_MAG1] != ELFMAG1 ||
+        hdr->e_ident[EI_MAG2] != ELFMAG2 ||
+        hdr->e_ident[EI_MAG3] != ELFMAG3
+    ) {
+        return false;
+    }
+
+    return true;
+}
+
+static inline void *elf_load_rel(Elf32_Ehdr *hdr)
+{
+    int result;
+
+    result = elf_load_stage1(hdr);
+}
+
+void *elf_load_file(void *file)
+{
+    Elf32_Ehdr *hdr = (Elf32_Ehdr*) file;
+
+    if (!check_elf_header(hdr)) {
+        return NULL;
+    }
+
+    switch(hdr->e_type) {
+        case ET_EXEC:
+            break;
+        case ET_REL:
+            break;
+        default:
+            break;
+    }
+
+    return NULL;
+}
--- programs/program.nasm
+++ programs/program.nasm
@@ -5,7 +5,7 @@ section .text
 
 mov eax, dword 4
 mov ebx, testString
-mov ecx, 13
+mov ecx, 12
 int 0x80
 
 mov eax, dword 4
